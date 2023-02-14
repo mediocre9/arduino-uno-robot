b5e8867604b2d7f2e4e83d09c4f6da9f397b34c3
mediocre9 <mirzafahadzia9@gmail.com>
2023-02-14 13:48:25 +0500

    refac/feat: add `singleton` pattern

--- robot_arduino_uno.ino
+++ robot_arduino_uno.ino
@@ -5,11 +5,17 @@
 * Note: We are currently using [Songle] (manufacturer) relay to control robot's 
 * movement. Other [Tongling] (manufacturer) relay will be used for other robot operations...
 * 
+* @To-do:
+* Memory Optimization [Done]
+* Refactoring [ ]
+*
+* Note: 
+* Please redeclare/recheck values here, according to arduino board...
 */
 
 
 // Red 4-channel relay. . .
-#define SONGLE_RELAY_INI_1 4  // broken pin here . . . brown pair
+#define SONGLE_RELAY_INI_1 4  
 #define SONGLE_RELAY_INI_2 7
 #define SONGLE_RELAY_INI_3 5
 #define SONGLE_RELAY_INI_4 6
@@ -21,32 +27,47 @@
 #define TONGLING_RELAY_INI_4 9
 #define BAUD_RATE 9600
 
+
+// Set Flag here for log outputs
 #define DEBUG_MODE 1
 
 
-const char FORWARD = 'F';
+const char FORWARD  = 'F';
 const char BACKWARD = 'B';
-const char LEFT = 'L';
-const char RIGHT = 'R';
-const char STOP = 'X';
+const char LEFT     = 'L';
+const char RIGHT    = 'R';
+const char STOP     = 'X';
+const char CAMERA_VIEW_UP    = 'W';
+const char CAMERA_VIEW_DOWN  = 'S';
+const char CAMERA_VIEW_LEFT  = 'D';
+const char CAMERA_VIEW_RIGHT = 'A';
 
 
-// Singleton robot class . . .
+
+/*
+* Singleton Class:
+* For One point object reference . . .
+*/
 class Robot {
 public:
   static Robot* init() {
     if (instance_ == nullptr) {
       instance_ = new Robot();
     }
-    control();
     return instance_;
   }
 
   static void control() {
     if (Serial.available() > 0) {
       bluetoothResponse_ = Serial.read();
+      
+#if DEBUG_MODE
       Serial.print(bluetoothResponse_);
+#endif
+
       switch (bluetoothResponse_) {
+
+        // for robot movement . . . 
         case FORWARD:
           move_forward();
           break;
@@ -63,10 +84,24 @@ public:
           move_right();
           break;
 
+          // for camera angle view . . . 
+        case CAMERA_VIEW_UP:
+          move_camera_angle_to_up();
+          break;
+
+        case CAMERA_VIEW_DOWN:
+          move_camera_angle_to_down();
+          break;
+
+        case CAMERA_VIEW_LEFT:
+          move_camera_angle_to_left();
+          break;
+
+        case CAMERA_VIEW_RIGHT:
+          move_camera_angle_to_right();
+          break;
+
         default:
-#if DEBUG_MODE
-          Serial.print(bluetoothResponse_);
-#endif
           break;
       }
     }
@@ -76,17 +111,15 @@ private:
   Robot() {}
 
   static void move_left() {
-    digitalWrite(TONGLING_RELAY_INI_1, LOW);
-    digitalWrite(TONGLING_RELAY_INI_2, LOW);
-    digitalWrite(TONGLING_RELAY_INI_3, LOW);
-    digitalWrite(TONGLING_RELAY_INI_4, LOW);
+    /**
+     * Do stuff from here . . . 
+     */
   }
 
   static void move_right() {
-    digitalWrite(TONGLING_RELAY_INI_1, HIGH);
-    digitalWrite(TONGLING_RELAY_INI_2, HIGH);
-    digitalWrite(TONGLING_RELAY_INI_3, HIGH);
-    digitalWrite(TONGLING_RELAY_INI_4, HIGH);
+    /**
+     * Do stuff from here . . . 
+     */
   }
 
   static void move_forward() {
@@ -101,6 +134,31 @@ private:
     digitalWrite(SONGLE_RELAY_INI_2, LOW);
     digitalWrite(SONGLE_RELAY_INI_3, LOW);
     digitalWrite(SONGLE_RELAY_INI_4, LOW);
+  }
+
+  // camera angle movement. . .   
+  static void move_camera_angle_to_up() {
+    /**
+     * Do stuff from here . . . 
+     */
+  }
+
+  static void move_camera_angle_to_down() {
+    /**
+     * Do stuff from here . . . 
+     */
+  }
+
+  static void move_camera_angle_to_left() {
+    /**
+     * Do stuff from here . . . 
+     */
+  }
+
+  static void move_camera_angle_to_right() {
+    /**
+     * Do stuff from here . . . 
+     */
   }
 
 private:
@@ -129,5 +187,5 @@ void setup() {
 }
 
 void loop() {
-  Robot::init();
+  Robot::init()->control();
 }
